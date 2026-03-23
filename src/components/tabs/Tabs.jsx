fffb619130babd6a2e9f613511438c2d2f55f671
Nileshnhe <nehenilesh111@gmail.com>
2026-03-23 09:44:32 +0530

    feat: implement dynamic tabs with state management

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./tabs.css"
 
 const Tabs = () => {
+
+  const [selectedTab, setSelectedTab] = useState("All")
+  const tabs = ["All", "Active", "Completed"]
   return (
     <div className='tabs-container'>
       <ul className='tabs-list'>
-        <li>All</li>
-        <li>Active</li>
-        <li>Completed</li>
+        {tabs.map((tab) => (
+          <li 
+          className={selectedTab === tab ? `active` : ``}
+          onClick={() => setSelectedTab(tab)}
+          >
+            {tab}
+            </li>
+        ))}
       </ul>
     </div>
   )
